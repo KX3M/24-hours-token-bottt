f156053a1213fc06659df3fc20a3d80f4cd28414
D4RK ‼️ <116873243+atiiif@users.noreply.github.com>
2024-03-04 14:57:40 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -121,9 +121,16 @@ async def start_command(client: Client, message: Message):
 
         elif verify_status['is_verified']:
             reply_markup = InlineKeyboardMarkup(
-                [[InlineKeyboardButton("🤖 About Me", callback_data="about"),
-                  InlineKeyboardButton("🔒 Close", callback_data="close")]]
-            )
+            [          
+                [
+                    InlineKeyboardButton("⚡ MAIN CHANNEL ⚡", url = "https://t.me/+Nz4u9JnJ5VMxMmU9")
+                ],
+                [
+                    InlineKeyboardButton("🤖 About Me", callback_data = "about"),
+                    InlineKeyboardButton("🔒 Close", callback_data = "close")
+                ]
+            ]
+        )
             await message.reply_text(
                 text=START_MSG.format(
                     first=message.from_user.first_name,
