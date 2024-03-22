db2bc790671caab552625f5e7255a1f488714e15
D4RK ‼️ <116873243+atiiif@users.noreply.github.com>
2024-03-22 12:02:15 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -140,8 +140,8 @@ async def start_command(client: Client, message: Message):
         else:
             verify_status = await get_verify_status(id)
             if IS_VERIFY and not verify_status['is_verified']:
-                short_url = f"api.shareus.io"
-                full_tut_url = f"https://t.me/linkshortenerx"
+                short_url = f"api.publicearn.com"
+                full_tut_url = f"https://t.me/BJPtutorials/6"
                 token = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
                 await update_verify_status(id, verify_token=token, link="")
                 link = await get_shortlink(SHORTLINK_URL, SHORTLINK_API,f'https://telegram.dog/{client.username}?start=verify_{token}')
