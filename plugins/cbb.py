9e170e5b506e6b47beaafdcac77c963e2aacd912
KX3M <163962831+KX3M@users.noreply.github.com>
2024-04-11 02:48:27 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : @oftenes</b>",
+            text = f"<b>🤖 Owner : @lnkachips \n 👨‍💻 Devloper : @ifeelscam</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
