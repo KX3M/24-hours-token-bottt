50cfd8c98c3aba19dac0adcf6634b06fb5c4964d
KX3M <163962831+KX3M@users.noreply.github.com>
2024-06-25 16:43:34 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -24,24 +24,27 @@ async def cb_handler(client: Bot, query: CallbackQuery):
                 ]
             )
         )
-    elif data == "close":
-        await query.message.delete()
-        try:
-            await query.message.reply_to_message.delete()
-        except: 
-            pass
+
     elif data == "buy_prem":
         await query.message.edit_text(
-            text=f"👋 {query.from_user.username}\n\n🎖️ Available Plans :\n\n● {PRICE1} rs For 7 Days Prime Membership\n\n● {PRICE2} rs For 1 Month Prime Membership\n\n● {PRICE3} rs For 3 Months Prime Membership\n\n● {PRICE4} rs For 6 Months Prime Membership\n\n● {PRICE5} rs For 1 Year Prime Membership\n\n\n💵 UPI ID -  {UPI_ID}\n\n\n📸 QR - [ᴄʟɪᴄᴋ ʜᴇʀᴇ ᴛᴏ ꜱᴄᴀɴ]({UPI_IMAGE_URL})\n\n♻️ If payment is not getting sent on above given QR code then inform admin, he will give you new QR code\n\n\n‼️ Must Send Screenshot after payment",
+            text=f"👋 Hello {mention}\n\n🎖️ Available Plans :\n\n● ₹{PRICE1} For 7 Days Prime Membership\n\n● ₹{PRICE2} For 1 Month Prime Membership\n\n● ₹{PRICE3} For 3 Months Prime Membership\n\n● ₹{PRICE4} For 6 Months Prime Membership\n\n● ₹{PRICE5} For 1 Year Prime Membership\n\n\n🔖 If you want to purchase Prime membership then please Contact Bot owner\n\nOwner & Admin accounts are mentioned below \n ⬇️⬇️👇👇⬇️⬇️",
             disable_web_page_preview=True,
             reply_markup = InlineKeyboardMarkup(
                 [   
                     [
-                        InlineKeyboardButton("Send Payment Screenshot(ADMIN) 📸", url=(SCREENSHOT_URL))
+                        InlineKeyboardButton("Bot Owner", url="t.me/inkax"),
+                        InlineKeyboardButton("Bot Admin",url=(SCREENSHOT_URL))
                     ],
                     [
                         InlineKeyboardButton("🔒 Close", callback_data = "close")
                     ]
                 ]
             )
-            )
+        )
+    elif data == "close":
+        await query.message.delete()
+        try:
+            await query.message.reply_to_message.delete()
+        except: 
+            pass
+    
